b7dd828a10800a9817cc8bf0c92e7172d05d868c
Dabaihero <493546531@qq.com>
2023-05-10 17:28:03 +0800

    Initial commit

---
+++ feign/src/main/java/com/dabai/feign/impl/FeignError.java
@@ -0,0 +1,20 @@
+package com.dabai.feign.impl;
+
+import com.dabai.entity.Student;
+import com.dabai.feign.FeignProviderClient;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class FeignError implements FeignProviderClient {
+    @Override
+    public Collection<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public String index() {
+        return "服务器维护中......";
+    }
+}
